ace3dd3c8750defb7fcb81fc618d5f51a6b6b4af
Daniel Kuroski <daniel.kuroski@gmail.com>
2020-09-27 18:06:48 +0200

    Add basic mov to gif convertion

--- api/fileUpload.js
+++ api/fileUpload.js
@@ -1,9 +1,38 @@
-// const gify = require("gify");
+const fs = require("fs");
+const multiparty = require("multiparty");
+var ffmpeg = require("fluent-ffmpeg");
 
-module.exports = (req, res) => {
-  res.json({
-    body: req.body,
-    query: req.query,
-    cookies: req.cookies
-  });
+const allowCors = fn => async (req, res) => {
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+  );
+  if (req.method === "OPTIONS") {
+    res.status(200).end();
+    return;
+  }
+  return await fn(req, res);
 };
+
+module.exports = allowCors((req, res) => {
+  const form = new multiparty.Form();
+
+  form.parse(req, function(_err, _fields, files) {
+    ffmpeg(files.file[0].path)
+      .on("end", function() {
+        res.setHeader("Content-disposition", "attachment; filename=output.gif");
+        const readStream = fs.createReadStream("./output.gif");
+        readStream.on("open", function() {
+          res.setHeader("Content-type", "image/gif");
+          readStream.pipe(res);
+        });
+      })
+      .save("./output.gif");
+  });
+});
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,19 @@
 <template>
   <div class="home">
+    <input
+      type="file"
+      name="file"
+      :disabled="isSaving"
+      @change="filesChange($event.target.name, $event.target.files)"
+      accept="*"
+      class="input-file"
+    />
+    <p v-if="isInitial">
+      Drag your file(s) here to begin<br />
+      or click to browse
+    </p>
+    <p v-if="isSaving">Uploadingfiles...</p>
+
     <div v-if="state.matches('idle')">
       idle
     </div>
@@ -16,12 +30,41 @@
     <button @click="send('UPLOAD', { data: 'MYBLOB' })">
       Send
     </button>
+
+    <img v-if="src" :src="src" />
   </div>
 </template>
 
 <script>
 import { useMachine } from "@xstate/vue";
 import gifMachine from "@/gifMachine";
+
+const STATUS_INITIAL = 0,
+  STATUS_SAVING = 1,
+  STATUS_SUCCESS = 2,
+  STATUS_FAILED = 3;
+
+function upload(formData, cb) {
+  const url = `http://localhost:3000/api/fileUpload`;
+  return (
+    fetch(url, {
+      method: "POST",
+      body: formData
+    })
+      .then(x => x.blob())
+      // add url field
+      .then(blobResponse => {
+        console.log(blobResponse);
+        let reader = new FileReader();
+        reader.readAsDataURL(blobResponse);
+        reader.onloadend = function() {
+          let base64data = reader.result;
+          console.log(base64data);
+          cb(base64data);
+        };
+      })
+  );
+}
 
 export default {
   name: "Home",
@@ -31,6 +74,97 @@ export default {
       state,
       send
     };
+  },
+  data() {
+    return {
+      uploadedFiles: [],
+      uploadError: null,
+      currentStatus: null,
+      src: null
+    };
+  },
+  computed: {
+    isInitial() {
+      return this.currentStatus === STATUS_INITIAL;
+    },
+    isSaving() {
+      return this.currentStatus === STATUS_SAVING;
+    },
+    isSuccess() {
+      return this.currentStatus === STATUS_SUCCESS;
+    },
+    isFailed() {
+      return this.currentStatus === STATUS_FAILED;
+    }
+  },
+  methods: {
+    reset() {
+      // reset form to initial state
+      this.currentStatus = STATUS_INITIAL;
+      this.uploadedFiles = [];
+      this.uploadError = null;
+    },
+    save(formData) {
+      // upload data to the server
+      this.currentStatus = STATUS_SAVING;
+
+      upload(formData, url => {
+        this.src = url;
+      })
+        .then(x => {
+          this.uploadedFiles = [].concat(x);
+          this.currentStatus = STATUS_SUCCESS;
+        })
+        .catch(err => {
+          this.uploadError = err.response;
+          this.currentStatus = STATUS_FAILED;
+        });
+    },
+    filesChange(fieldName, fileList) {
+      // handle file changes
+      const formData = new FormData();
+
+      if (!fileList.length) return;
+
+      formData.append(fieldName, fileList[0], fileList[0].name);
+
+      // save it
+      this.save(formData);
+    }
+  },
+  mounted() {
+    this.reset();
   }
 };
 </script>
+
+<style>
+.dropbox {
+  outline: 2px dashed grey; /* the dash box */
+  outline-offset: -10px;
+  background: lightcyan;
+  color: dimgray;
+  padding: 10px 10px;
+  min-height: 200px; /* minimum height */
+  position: relative;
+  cursor: pointer;
+}
+
+.input-file {
+  opacity: 0; /* invisible but it's there! */
+  width: 100%;
+  height: 200px;
+  position: absolute;
+  cursor: pointer;
+}
+
+.dropbox:hover {
+  background: lightblue; /* when mouse over to the drop zone, change color */
+}
+
+.dropbox p {
+  font-size: 1.2em;
+  text-align: center;
+  padding: 50px 0;
+}
+</style>
